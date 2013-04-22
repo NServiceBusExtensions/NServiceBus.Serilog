eb2f58f300310ad8dd9a13408b0f2b12250dfdb0
Simon Cropp <simon.cropp@gmail.com>
2013-04-22 22:55:55 +1000

    doco

--- NServiceBusSerilog/SerilogConfigurator.cs
+++ NServiceBusSerilog/SerilogConfigurator.cs
@@ -3,13 +3,24 @@ using Serilog;
 
 namespace NServiceBusSerilog
 {
+    /// <summary>
+    /// Used to forward NServiceBus logging message through Serilog
+    /// </summary>
     public static class SerilogConfigurator
     {
+        /// <summary>
+        /// Configur NServiceBus logging messages to use Serilog
+        /// </summary>
+        /// <remarks>The default <see cref="Log.Logger"/> is used as the target for messages.</remarks>
         public static void Configure()
         {
             Configure(Log.Logger);
         }
 
+        /// <summary>
+        /// Configur NServiceBus logging messages to use Serilog
+        /// </summary>
+        /// <param name="logger">The <see cref="ILogger"/> to use as the target for messages.</param>
         public static void Configure(ILogger logger)
         {
             LogManager.LoggerFactory = new LoggerFactory(logger);
