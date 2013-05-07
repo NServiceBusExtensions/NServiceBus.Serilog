6f7ba57e8237f4469857f6775f21de12b6ec11ab
Simon Cropp <simon.cropp@gmail.com>
2013-05-07 20:51:38 +1000

    version

--- NServiceBus.Serilog/AssemblyInfo.cs
+++ NServiceBus.Serilog/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("NServiceBusSerilog")]
-[assembly: AssemblyProduct("NServiceBusSerilog")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyTitle("NServiceBus.Serilog")]
+[assembly: AssemblyProduct("NServiceBus.Serilog")]
+[assembly: AssemblyVersion("0.1.3.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
