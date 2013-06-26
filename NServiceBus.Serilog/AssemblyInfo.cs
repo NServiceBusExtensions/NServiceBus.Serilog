4df6280e82259262b3d7b79d37562f54dc292a51
Simon Cropp <simon.cropp@gmail.com>
2013-06-26 16:33:58 +1000

    update refs

--- NServiceBus.Serilog/AssemblyInfo.cs
+++ NServiceBus.Serilog/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NServiceBus.Serilog")]
 [assembly: AssemblyProduct("NServiceBus.Serilog")]
-[assembly: AssemblyVersion("0.1.5.0")]
-[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
