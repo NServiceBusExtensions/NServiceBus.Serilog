3e45b5f8af898845d8c53b6ae6fae471278c33ce
Simon Cropp <simon.cropp@gmail.com>
2014-05-11 20:07:27 +1000

    fix versioning

--- NServiceBus.Serilog.Tracing/AssemblyInfo.cs
+++ NServiceBus.Serilog.Tracing/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.Reflection;
 
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: AssemblyTitle("NServiceBus.Serilog.Tracing")]
 [assembly: AssemblyProduct("NServiceBus.Serilog.Tracing")]
--- NServiceBus.Serilog/AssemblyInfo.cs
+++ NServiceBus.Serilog/AssemblyInfo.cs
@@ -1,5 +1,6 @@
-﻿
-using System.Reflection;
+﻿using System.Reflection;
 
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 [assembly: AssemblyTitle("NServiceBus.Serilog")]
 [assembly: AssemblyProduct("NServiceBus.Serilog")]
