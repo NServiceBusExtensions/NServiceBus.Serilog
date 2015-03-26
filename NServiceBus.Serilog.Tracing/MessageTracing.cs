05db2ce70aef9a6fecba6d0315132b610a901901
Jon Scheiding <jscheiding@pureromance.com>
2015-03-26 16:45:56 -0400

    Breaking message audit and saga audit into separate features to allow use in endpoints that don't have any sagas

---
+++ NServiceBus.Serilog.Tracing/MessageTracing.cs
@@ -0,0 +1,37 @@
+﻿using NServiceBus.Features;
+using NServiceBus.Pipeline;
+
+namespace NServiceBus.Serilog.Tracing {
+    public class MessageTracing : Feature
+    {
+        public MessageTracing() 
+        {
+            EnableByDefault();
+            DependsOn<TracingLog>();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context) 
+        {
+            context.Pipeline.Register<ReceiveMessageRegistration>();
+            context.Pipeline.Register<SendMessageRegistration>();
+        }
+
+        class ReceiveMessageRegistration : RegisterStep 
+        {
+            public ReceiveMessageRegistration()
+                : base("SerilogReceiveMessage", typeof(ReceiveMessageBehavior), "Logs incoming messages") 
+            {
+                InsertBefore(WellKnownStep.MutateIncomingMessages);
+            }
+        }
+
+        class SendMessageRegistration : RegisterStep 
+        {
+            public SendMessageRegistration()
+                : base("SerilogSendMessage", typeof(SendMessageBehavior), "Logs outgoing messages") 
+            {
+                InsertAfter(WellKnownStep.DispatchMessageToTransport);
+            }
+        }
+    }
+}
