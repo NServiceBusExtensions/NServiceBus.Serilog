2c4c3fb9850ff4f2081fa877722615bdf54a3373
Simon Cropp <simon.cropp@gmail.com>
2018-10-31 15:12:13 +1100

    scrub directory

---
+++ Tests/AssemblyLocation.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+public static class AssemblyLocation
+{
+    static AssemblyLocation()
+    {
+        var assembly = typeof(AssemblyLocation).Assembly;
+
+        var path = assembly.Location
+            .Replace("file:///", "")
+            .Replace("file://", "")
+            .Replace(@"file:\\\", "")
+            .Replace(@"file:\\", "");
+
+
+        SourceDirectory = new DirectoryInfo(Path.GetDirectoryName(path)).Parent.Parent.Parent.FullName;
+    }
+
+    public static string SourceDirectory;
+}
--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -73,7 +73,11 @@ public class IntegrationTests : TestBase
                     logsWithExceptions = logEvents.LogsWithExceptions().ToList()
                 },
                 jsonSerializerSettings: null,
-                scrubber: s => s.Replace(Environment.MachineName, "MachineName"));
+                scrubber: s =>
+                {
+                    var replace = s.Replace(Environment.MachineName, "MachineName");
+                    return replace.ReplaceCaseless(AssemblyLocation.SourceDirectory.Replace(@"\",@"\\"), "CurrentDirectory");
+                });
         }
     }
 
