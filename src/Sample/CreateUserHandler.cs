424603a1777ab2bf414b772b84aee87d11249c4d
Simon Cropp <simon.cropp@gmail.com>
2019-03-19 10:07:04 +1100

    add extra message to sample

--- src/Sample/CreateUserHandler.cs
+++ src/Sample/CreateUserHandler.cs
@@ -1,12 +1,13 @@
 ﻿using System.Threading.Tasks;
 using NServiceBus;
 
-public class CreateUserHandler : IHandleMessages<CreateUser>
+public class CreateUserHandler :
+    IHandleMessages<CreateUser>
 {
     public Task Handle(CreateUser message, IMessageHandlerContext context)
     {
         var logger = context.Logger();
         logger.Information("Hello from {@Handler}.");
-        return Task.CompletedTask;
+        return context.SendLocal(new UserCreated());
     }
 }
