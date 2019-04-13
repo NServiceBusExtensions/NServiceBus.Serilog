08622c3c16964a8273f0d492f11a0b1342b19909
Simon Cropp <simon.cropp@gmail.com>
2019-04-13 19:19:58 +1000

    update refs

--- src/Tests/TestConfig/CustomContractResolverEx.cs
+++ src/Tests/TestConfig/CustomContractResolverEx.cs
@@ -1,11 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using ObjectApproval;
 
 public class CustomContractResolverEx : CustomContractResolver
 {
-    public CustomContractResolverEx() : base(true)
+    public CustomContractResolverEx() :
+        base(true, true, new Dictionary<Type, List<string>>(), new List<Type>(), new List<Func<Exception, bool>>())
     {
     }
 
