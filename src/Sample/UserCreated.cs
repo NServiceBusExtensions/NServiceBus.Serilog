f3246d7249a3b0ef06a895e7834df0bb6603c4be
Simon Cropp <simon.cropp@gmail.com>
2019-09-28 13:38:53 +1000

    nullables

--- src/Sample/UserCreated.cs
+++ src/Sample/UserCreated.cs
@@ -3,7 +3,7 @@
 public class UserCreated :
     IMessage
 {
-    public string UserName { get; set; }
-    public string FamilyName { get; set; }
-    public string GivenNames{ get; set; }
+    public string? UserName { get; set; }
+    public string? FamilyName { get; set; }
+    public string? GivenNames{ get; set; }
 }
