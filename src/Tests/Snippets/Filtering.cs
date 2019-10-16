079626188087fadd3e43fc88a79fc19ef1192cf0
Simon Cropp <simon.cropp@gmail.com>
2019-10-16 21:35:01 +1100

    docs

---
+++ src/Tests/Snippets/Filtering.cs
@@ -0,0 +1,26 @@
+﻿using NServiceBus.Logging;
+using NServiceBus.Serilog;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
+
+public class Filtering
+{
+    public Filtering()
+    {
+        #region SerilogFiltering
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(
+                path:"log.txt",
+                restrictedToMinimumLevel: LogEventLevel.Debug
+            )
+            .Filter.ByIncludingOnly(
+                inclusionPredicate: Matching.FromSource("MyNamespace"))
+            .CreateLogger();
+
+        LogManager.Use<SerilogFactory>();
+
+        #endregion
+    }
+}
---
+++ src/Tests/Snippets/Usage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NServiceBus.Logging;
+using NServiceBus.Serilog;
+using Serilog;
+
+class Usage
+{
+    Usage()
+    {
+        #region SerilogInCode
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File("log.txt")
+            .CreateLogger();
+
+        LogManager.Use<SerilogFactory>();
+
+        #endregion
+    }
+
+    void Seq()
+    {
+        #region SerilogSeq
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Seq("http://localhost:5341")
+            .MinimumLevel.Information()
+            .CreateLogger();
+
+        LogManager.Use<SerilogFactory>();
+
+        #endregion
+    }
+
+    void ExceptionLogState(Exception exception)
+    {
+        #region ExceptionLogState
+
+        var data = exception.Data;
+        if (data.Contains("ExceptionLogState"))
+        {
+            var logState = (ExceptionLogState) data["ExceptionLogState"]!;
+            var endpoint = logState.ProcessingEndpoint;
+            var incomingMessageId = logState.IncomingMessageId;
+            var incomingMessageType = logState.IncomingMessageType;
+            var correlationId = logState.CorrelationId;
+            var conversationId = logState.ConversationId;
+            var handlerType = logState.HandlerType;
+            var incomingHeaders = logState.IncomingHeaders;
+            var incomingMessage = logState.IncomingMessage;
+        }
+
+        #endregion
+    }
+}
