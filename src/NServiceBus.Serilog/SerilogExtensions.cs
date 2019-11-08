203f9311296c3ac244effdfff895ce0cd651cb25
Simon Cropp <simon.cropp@gmail.com>
2019-11-08 20:21:03 +1100

    use NotNullWhen

--- src/NServiceBus.Serilog/SerilogExtensions.cs
+++ src/NServiceBus.Serilog/SerilogExtensions.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Serilog;
 using Serilog.Events;
 
 static class SerilogExtensions
 {
-    public static bool BindProperty(this ILogger logger, string name, object value, out LogEventProperty property)
+    public static bool BindProperty(
+        this ILogger logger,
+        string name,
+        object value,
+        [NotNullWhen(true)] out LogEventProperty? property)
     {
         return logger.BindProperty(name, value, true, out property);
     }
 
-    public static void WriteInfo(this ILogger logger, MessageTemplate messageTemplate, IEnumerable<LogEventProperty> properties)
+    public static void WriteInfo(
+        this ILogger logger,
+        MessageTemplate messageTemplate,
+        IEnumerable<LogEventProperty> properties)
     {
         var logEvent = new LogEvent(
             timestamp: DateTimeOffset.Now,
