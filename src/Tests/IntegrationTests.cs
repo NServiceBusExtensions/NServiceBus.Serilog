8ed5a9085216da6ea8c804d28ab273e7e26a3bbc
Simon Cropp <simon.cropp@gmail.com>
2020-07-10 21:58:04 +1000

    cleanup

--- src/Tests/IntegrationTests.cs
+++ src/Tests/IntegrationTests.cs
@@ -9,6 +9,7 @@ using NServiceBus.Serilog;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
+using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
@@ -94,13 +95,15 @@ public class IntegrationTests
     Task Verify<T>(List<LogEventEx> logEvents)
     {
         var logsForTarget = logEvents.LogsForType<T>().ToList();
+        var settings = new VerifySettings();
+        settings.ScrubLinesContaining("NServiceBus.TimeSent");
         return Verifier.Verify(
             new
             {
                 logsForTarget,
                 logsForNsbSerilog = logEvents.LogsForNsbSerilog().ToList(),
                 logsWithExceptions = logEvents.LogsWithExceptions().ToList()
-            });
+            },settings);
     }
 
     static async Task<IEnumerable<LogEventEx>> Send(object message, Action<SendOptions>? optionsAction = null)
