e7856f78331a4a533ac8e18818eb63e92a1db029
Simon Cropp <simon.cropp@gmail.com>
2020-11-30 11:10:11 +1100

    make TypeNameConverter public

--- src/NServiceBus.Serilog/TypeNameConverter.cs
+++ src/NServiceBus.Serilog/TypeNameConverter.cs
@@ -5,112 +5,117 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.CSharp;
 
-static class TypeNameConverter
+namespace NServiceBus.Serilog
 {
-    static ConcurrentDictionary<Type, string> typeToNameCache = new();
-    static ConcurrentDictionary<string, string> longNameToNameCache = new();
-
-    static CSharpCodeProvider codeDomProvider = new();
-
-    public static string GetName(string longName)
+    public static class TypeNameConverter
     {
-        return longNameToNameCache.GetOrAdd(longName, Inner);
-    }
+        static ConcurrentDictionary<Type, string> typeToNameCache = new();
+        static ConcurrentDictionary<string, string> longNameToNameCache = new();
 
-    static string Inner(string longName)
-    {
-        Type? type;
-        try
+        static CSharpCodeProvider codeDomProvider = new();
+
+        public static string GetName(string longName)
         {
-            type = Type.GetType(longName);
-        }
-        catch
-        {
-            return longName;
+            return longNameToNameCache.GetOrAdd(longName, Inner);
         }
 
-        if (type == null)
+        static string Inner(string longName)
         {
-            return longName;
-        }
-
-        return GetName(type);
-    }
-
-    public static string GetName(Type type)
-    {
-        return typeToNameCache.GetOrAdd(type, Inner);
-    }
-
-    static string Inner(Type type)
-    {
-        if (IsAnonType(type))
-        {
-            return "dynamic";
-        }
-
-        if (type.Name.StartsWith("<") ||
-            type.IsNested && type.DeclaringType == typeof(Enumerable))
-        {
-            var singleOrDefault = type.GetInterfaces()
-                .SingleOrDefault(x =>
-                    x.IsGenericType &&
-                    x.GetGenericTypeDefinition() == typeof(IEnumerable<>));
-            if (singleOrDefault != null)
+            Type? type;
+            try
             {
-                if (singleOrDefault.GetGenericArguments().Single().IsAnonType())
-                {
-                    return "IEnumerable<dynamic>";
-                }
-                return GetName(singleOrDefault);
+                type = Type.GetType(longName);
             }
+            catch
+            {
+                return longName;
+            }
+
+            if (type == null)
+            {
+                return longName;
+            }
+
+            return GetName(type);
         }
 
-        var typeName = GetTypeName(type);
-        CodeTypeReference reference = new(typeName);
-        var name = codeDomProvider.GetTypeOutput(reference);
-        List<string> list = new();
-        AllGenericArgumentNamespace(type, list);
-        foreach (var ns in list.Distinct())
+        public static string GetName(Type type)
         {
-            name = name.Replace($"<{ns}.", "<");
-            name = name.Replace($", {ns}.", ", ");
+            return typeToNameCache.GetOrAdd(type, Inner);
         }
 
-        return name;
-    }
-
-    static string GetTypeName(Type type)
-    {
-        if (type.FullName == null)
+        static string Inner(Type type)
         {
-            return type.Name;
+            if (IsAnonType(type))
+            {
+                return "dynamic";
+            }
+
+            if (type.Name.StartsWith("<") ||
+                type.IsNested && type.DeclaringType == typeof(Enumerable))
+            {
+                var singleOrDefault = type.GetInterfaces()
+                    .SingleOrDefault(x =>
+                        x.IsGenericType &&
+                        x.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+                if (singleOrDefault != null)
+                {
+                    if (singleOrDefault.GetGenericArguments().Single().IsAnonType())
+                    {
+                        return "IEnumerable<dynamic>";
+                    }
+
+                    return GetName(singleOrDefault);
+                }
+            }
+
+            var typeName = GetTypeName(type);
+            CodeTypeReference reference = new(typeName);
+            var name = codeDomProvider.GetTypeOutput(reference);
+            List<string> list = new();
+            AllGenericArgumentNamespace(type, list);
+            foreach (var ns in list.Distinct())
+            {
+                name = name.Replace($"<{ns}.", "<");
+                name = name.Replace($", {ns}.", ", ");
+            }
+
+            return name;
         }
-        return type.FullName.Replace(type.Namespace + ".", "");
-    }
 
-    static bool IsAnonType(this Type type)
-    {
-        return type.Name.Contains("AnonymousType");
-    }
-
-    static void AllGenericArgumentNamespace(Type type, List<string> list)
-    {
-        if (type.Namespace != null)
+        static string GetTypeName(Type type)
         {
-            list.Add(type.Namespace);
+            if (type.FullName == null)
+            {
+                return type.Name;
+            }
+
+            return type.FullName.Replace(type.Namespace + ".", "");
         }
 
-        var elementType = type.GetElementType();
-
-        if (elementType != null)
+        static bool IsAnonType(this Type type)
         {
-            AllGenericArgumentNamespace(elementType,list);
+            return type.Name.Contains("AnonymousType");
         }
 
-        foreach (var generic in type.GenericTypeArguments)
+        static void AllGenericArgumentNamespace(Type type, List<string> list)
         {
-            AllGenericArgumentNamespace(generic, list);
+            if (type.Namespace != null)
+            {
+                list.Add(type.Namespace);
+            }
+
+            var elementType = type.GetElementType();
+
+            if (elementType != null)
+            {
+                AllGenericArgumentNamespace(elementType, list);
+            }
+
+            foreach (var generic in type.GenericTypeArguments)
+            {
+                AllGenericArgumentNamespace(generic, list);
+            }
         }
     }
 }
