264b79d1a58b1d23269a3415011c8f0ba0cefbfa
Simon Cropp <simon.cropp@gmail.com>
2021-01-18 16:07:00 +1100

    Update ConvertHeader.cs

--- src/NServiceBus.Serilog/ConvertHeader.cs
+++ src/NServiceBus.Serilog/ConvertHeader.cs
@@ -1,3 +1,6 @@
 using Serilog.Events;
 
+/// <summary>
+/// Used to support writing a custom log property for a specific header.
+/// </summary>
 public delegate LogEventProperty? ConvertHeader(string key, string value);
