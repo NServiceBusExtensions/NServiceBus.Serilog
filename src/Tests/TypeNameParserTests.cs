bcef531d2bac154e9f8e287e55f499d1ee5e935f
Simon Cropp <simon.cropp@gmail.com>
2021-04-15 11:56:19 +1000

    .

--- src/Tests/TypeNameParserTests.cs
+++ src/Tests/TypeNameParserTests.cs
@@ -1,21 +1,42 @@
 ﻿using System.Threading.Tasks;
-using VerifyTests;
 using VerifyXunit;
 using Xunit;
 
 [UsesVerify]
 public class TypeNameParserTests
 {
-    [Theory]
-    [InlineData("TheName")]
-    [InlineData("Namespace.TheName")]
-    [InlineData("Namespace.TheName, Assembly, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
-    [InlineData("MyMessage`1[[System.Int32, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]], Sample, Version=0.0.0.0, Culture=neutral, PublicKeyToken=ce8ec7717ba6fbb6")]
-    public Task InlineDataUsage(string name)
+    [Fact]
+    public Task Simple()
     {
-        VerifySettings settings = new();
-        settings.UseParameters(name);
-        var typeName = TypeNameParser.ParseName(name,true,0,out _);
-        return Verifier.Verify(typeName, settings);
+        var typeName = TypeNameParser.ParseName("TheName", true, 0, out _);
+        return Verifier.Verify(typeName);
+    }
+
+    [Fact]
+    public Task Namespace()
+    {
+        var typeName = TypeNameParser.ParseName("Namespace.TheName", true, 0, out _);
+        return Verifier.Verify(typeName);
+    }
+
+    [Fact]
+    public Task AssemblyQualified()
+    {
+        var typeName = TypeNameParser.ParseName("Namespace.TheName, Assembly, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089", true, 0, out _);
+        return Verifier.Verify(typeName);
+    }
+
+    [Fact]
+    public Task GenericAssemblyQualified()
+    {
+        var typeName = TypeNameParser.ParseName("Namespace.MyMessage`1[[System.Int32, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]], Sample, Version=0.0.0.0, Culture=neutral, PublicKeyToken=ce8ec7717ba6fbb6", true, 0, out _);
+        return Verifier.Verify(typeName);
+    }
+
+    [Fact]
+    public Task Generic()
+    {
+        var typeName = TypeNameParser.ParseName("Namespace.MyMessage`1[[System.Int32]], Sample", true, 0, out _);
+        return Verifier.Verify(typeName);
     }
 }
