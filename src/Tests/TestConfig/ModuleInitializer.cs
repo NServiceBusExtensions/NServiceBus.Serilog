34c080540cd803c1ba92748fe2fc9ae7a0cf0de5
Simon Cropp <simon.cropp@gmail.com>
2022-02-03 10:43:22 +1100

    Update ModuleInitializer.cs

--- src/Tests/TestConfig/ModuleInitializer.cs
+++ src/Tests/TestConfig/ModuleInitializer.cs
@@ -26,14 +26,3 @@ public static class ModuleInitializer
         VerifierSettings.AddExtraDatetimeFormat("yyyy-MM-dd HH:mm:ss:ffffff Z");
     }
 }
-//Only required if using a legacy version of .net
-#if(!NET5_0)
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    public sealed class ModuleInitializerAttribute :
-        Attribute
-    {
-    }
-}
-#endif
