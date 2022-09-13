7ba3e4096c46b5cc2c3ab251e3f9880a39de1047
Simon <simon.cropp@gmail.com>
2022-09-13 14:41:14 +1000

    Update ScalarValueConverter.cs

--- src/Tests/TestConfig/ScalarValueConverter.cs
+++ src/Tests/TestConfig/ScalarValueConverter.cs
@@ -1,6 +1,11 @@
 ﻿class ScalarValueConverter :
     WriteOnlyJsonConverter<ScalarValue>
 {
-    public override void Write(VerifyJsonWriter writer, ScalarValue value) =>
-        writer.Serialize(value.Value);
+    public override void Write(VerifyJsonWriter writer, ScalarValue value)
+    {
+        if (value.Value != null)
+        {
+            writer.Serialize(value.Value);
+        }
+    }
 }
