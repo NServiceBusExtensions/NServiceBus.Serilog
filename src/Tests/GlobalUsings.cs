733aa39bc4a13cc3a227d7d8e1f15966f3dd4678
Simon Cropp <simon.cropp@gmail.com>
2025-07-10 12:01:27 +1000

    refs or cleanup

--- src/Tests/GlobalUsings.cs
+++ src/Tests/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿// Global using directives
-
-global using Microsoft.Extensions.DependencyInjection;
+﻿global using Microsoft.Extensions.DependencyInjection;
 global using NServiceBus.Testing;
 global using VerifyTests.NServiceBus;
